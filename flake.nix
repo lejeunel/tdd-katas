df131e11c3e5b9db38312d3642bf7b7d256d6144
lejeunel <me@lejeunel.org>
2026-02-16 05:48:39 +0100

    export catch2 path for clangd

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A Nix-flake devshell for TDD Katas in C++";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f: nixpkgs.lib.genAttrs supportedSystems (system: f { pkgs = import nixpkgs { inherit system; }; });
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+
+            packages = with pkgs; [
+              clang-tools
+              ccls
+              gnumake
+              cmake
+              gcc
+              catch2_3
+            ];
+            shellHook = ''
+              export CPATH="${pkgs.catch2_3}/include"
+            '';
+          };
+        }
+      );
+    };
+}
