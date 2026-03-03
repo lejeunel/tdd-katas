7ece7f31f35f3c9145d5a0e3e3fec0189ad5ae9b
lejeunel <me@lejeunel.org>
2026-03-03 16:27:30 +0100

    ohce: remove stopped session exception

--- ohce/src/exceptions.cpp
+++ ohce/src/exceptions.cpp
@@ -1,11 +1,5 @@
 #include "include/exceptions.h"
 
-StoppedSessionException::StoppedSessionException() {};
-
-const char *StoppedSessionException::what() const noexcept {
-  return "session is stopped";
-}
-
 SessionNotFoundException::SessionNotFoundException() {};
 
 const char *SessionNotFoundException::what() const noexcept {
--- ohce/src/include/exceptions.h
+++ ohce/src/include/exceptions.h
@@ -2,12 +2,6 @@
 #define EXCEPTIONS_H_
 #include <exception>
 
-class StoppedSessionException : public std::exception {
-public:
-  explicit StoppedSessionException();
-  const char *what() const noexcept override;
-};
-
 class SessionNotFoundException : public std::exception {
 public:
   explicit SessionNotFoundException();
